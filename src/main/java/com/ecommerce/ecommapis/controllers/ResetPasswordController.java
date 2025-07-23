dce956121769ea2da61e0e624b773386733b1b16
Kunal Nikhade <kunal.nikhade@gmail.com>
2025-07-23 16:53:52 +0530

    #12 : Implemented the reset password API's

---
+++ src/main/java/com/ecommerce/ecommapis/controllers/ResetPasswordController.java
@@ -0,0 +1,29 @@
+package com.ecommerce.ecommapis.controllers;
+
+import com.ecommerce.ecommapis.dto.ResetPasswordDto;
+import com.ecommerce.ecommapis.services.ResetPasswordService;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/reset-password")
+public class ResetPasswordController
+{
+    private final ResetPasswordService resetPasswordService;
+
+    public ResetPasswordController(ResetPasswordService resetPasswordService)
+    {
+        this.resetPasswordService = resetPasswordService;
+    }
+
+    @PostMapping(value = "/reset/{email}",
+                 consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+                 produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<String> resetPassword(@PathVariable final String email,
+                                                @RequestBody final ResetPasswordDto resetPasswordDto)
+    {
+        resetPasswordService.resetPassword(email, resetPasswordDto);
+
+        return new ResponseEntity<>("Reset password successfully", HttpStatus.OK);
+    }
+}
